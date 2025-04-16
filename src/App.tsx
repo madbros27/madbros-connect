99f49a8e1b459a926ea49e800d041bafe18e827d
Abhishek pruthvi <abhishek.pruthvi.vm@gmail.com>
2025-04-16 15:41:18 +0530

    port and context updated

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/madbros">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
